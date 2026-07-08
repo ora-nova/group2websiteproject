ea46f628cc9663bd95a218e3ab13419bb94d7953
ora-nova <shyakauwaseoranova@gmail.com>
2026-07-08 22:15:15 +0200

    configure multiple pages for vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [
+    tailwindcss(),
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        imprnt: resolve(__dirname, 'imprnt.html'),
+        menupage: resolve(__dirname, 'menupage.html'),
+        mainpage: resolve(__dirname, 'main.html'),
+      },
+    },
+  },
 })
